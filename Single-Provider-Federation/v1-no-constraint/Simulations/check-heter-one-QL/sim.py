dc6a3fa4fccf911045933a0a3c99b1bdcaa5373c
Bahador Bakhshi <bakhshi@gmail.com>
2020-10-07 01:57:53 +0330

    check-federation-cost-rand results

--- Single-Provider-Federation/v1-no-constraint/Simulations/check-heter-one-QL/sim.py
+++ Single-Provider-Federation/v1-no-constraint/Simulations/check-heter-one-QL/sim.py
@@ -12,7 +12,7 @@ import matplotlib.style
 import Environment
 import parser
 import DP
-from tester import test_greedy_random_policy, test_policy
+from tester import test_greedy_policy, test_policy
 from Environment import debug, error, warning
 
 if __name__ == "__main__":
@@ -22,8 +22,8 @@ if __name__ == "__main__":
 
     parser.parse_config("config.json")
 
-    init_size = 0.05
-    step = 0.025
+    init_size = 0.1
+    step = 0.05
     scale = 20
 
     i = 0
@@ -46,30 +46,26 @@ if __name__ == "__main__":
         debug("********* Optimal Policy ***********")
         #DP.print_policy(dp_policy)
     
+        env = Environment.Env(Environment.domain.total_cpu, sim_time)
+        ql_policy = QL.qLearning(env, episode_num)
+        debug("********* QL Policy ***********")
+        #DP.print_policy(ql_policy)
 
-        greedy_profit_0 = greedy_profit_50 = greedy_profit_100 = dp_profit = ql_profit = 0
+        greedy_profit = dp_profit = ql_profit = 0
 
         iterations = 30
         for j in range(iterations):
-            env = Environment.Env(Environment.domain.total_cpu, sim_time)
-            ql_policy = QL.qLearning(env, episode_num)
-            debug("********* QL Policy ***********")
-            #DP.print_policy(ql_policy)
         
             demands = Environment.generate_req_set(sim_time)
             Environment.print_reqs(demands)
 
-            greedy_profit_0 += test_greedy_policy(demands, 0) / float(len(demands))
-            greedy_profit_50 += test_greedy_policy(demands, 0.5) / float(len(demands))
-            greedy_profit_100 += test_greedy_policy(demands, 1) / float(len(demands))
+            greedy_profit += test_greedy_policy(demands) / float(len(demands))
 
             dp_profit += test_policy(demands, dp_policy) / float(len(demands))
         
             ql_profit += test_policy(demands, ql_policy) / float(len(demands))
 
         print("multiplier = ", m)
-        print("Greedy Profit = ", greedy_profit_0 / iterations)
-        print("Greedy Profit = ", greedy_profit_50 / iterations)
-        print("Greedy Profit = ", greedy_profit_100 / iterations)
+        print("Greedy Profit = ", greedy_profit / iterations)
         print("DP Profit = ", dp_profit / iterations)
         print("QL Profit = ", ql_profit / iterations)
---
+++ Single-Provider-Federation/v1-no-constraint/res_plotter_rand.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import json
+from numpy import *
+import math
+import matplotlib.pyplot as plt
+
+
+res_file_handler = open("res.json")
+res = json.load(res_file_handler)
+res_file_handler.close()
+
+x_label = res["x_label"]
+results = res["results"]
+
+x = []
+greedy_000 = []
+greedy_050 = []
+greedy_100 = []
+DP = []
+QL  = []
+
+for data in results:
+    x.append(data["x"])
+    greedy_000.append(data["greedy000"])
+    greedy_050.append(data["greedy050"])
+    greedy_100.append(data["greedy100"])
+    DP.append(data["DP"])
+    QL.append(data["QL"])
+
+
+fig, ax = plt.subplots()
+plt.grid(linestyle="--", linewidth=0.5)
+plt.ylabel("Normalized Profit")
+plt.xlabel(x_label)
+plt.plot(x, DP, label='DP', color='C4', linestyle='solid', linewidth=1.5)
+plt.plot(x, QL, label='QL', color='C0', linestyle='dashed', linewidth=1.5)
+plt.plot(x, greedy_000, label='Greedy-0.0', color='C1', linestyle='dashdot', linewidth=1.5)
+plt.plot(x, greedy_050, label='Greedy-0.5', color='C2', linestyle='dashdot', linewidth=1.5)
+plt.plot(x, greedy_100, label='Greedy-1.0', color='C3', linestyle='dashdot', linewidth=1.5)
+plt.legend(loc='best', handlelength=4)
+
+plt.show()
+plt.savefig("res.png")
