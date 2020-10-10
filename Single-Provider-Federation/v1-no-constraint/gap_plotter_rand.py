6af1c8203255108411e09fecab8b205cac30860f
Bahador Bakhshi <bakhshi@gmail.com>
2020-10-11 01:14:00 +0330

    Gap Plotter

--- Single-Provider-Federation/v1-no-constraint/gap_plotter_rand.py
+++ Single-Provider-Federation/v1-no-constraint/gap_plotter_rand.py
@@ -22,16 +22,28 @@ QL  = []
 
 for data in results:
     x.append(data["x"])
-    greedy_000.append(data["greedy000"])
-    greedy_050.append(data["greedy050"])
-    greedy_100.append(data["greedy100"])
-    DP.append(data["DP"])
-    QL.append(data["QL"])
+    greedy_000_tmp = data["greedy000"]
+    greedy_050_tmp = data["greedy050"]
+    greedy_100_tmp = data["greedy100"]
+    DP_tmp = data["DP"]
+    QL_tmp = data["QL"]
+    
+    greedy_000_tmp = (DP_tmp - greedy_000_tmp) / DP_tmp
+    greedy_050_tmp = (DP_tmp - greedy_050_tmp) / DP_tmp
+    greedy_100_tmp = (DP_tmp - greedy_100_tmp) / DP_tmp
+    QL_tmp = (DP_tmp - QL_tmp) / DP_tmp
+    DP_tmp = 0.0
+
+    greedy_000.append(greedy_000_tmp)
+    greedy_050.append(greedy_050_tmp)
+    greedy_100.append(greedy_100_tmp)
+    DP.append(DP_tmp)
+    QL.append(QL_tmp)
 
 
 fig, ax = plt.subplots()
 plt.grid(linestyle="--", linewidth=0.5)
-plt.ylabel("Average Profit")
+plt.ylabel("Gap")
 plt.xlabel(x_label)
 plt.plot(x, DP, label='DP', color='C4', linestyle='solid', linewidth=1.5)
 plt.plot(x, QL, label='QL', color='C0', linestyle='dashed', linewidth=1.5)
@@ -41,4 +53,4 @@ plt.plot(x, greedy_100, label='Greedy-1.0', color='C3', linestyle='dashdot', lin
 plt.legend(loc='best', handlelength=4)
 
 plt.show()
-plt.savefig("res.png")
+plt.savefig("gap.png")
