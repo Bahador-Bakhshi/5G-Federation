b5a1205f9574715aaace2404925719f53ca62719
Bahador Bakhshi <bakhshi@gmail.com>
2020-11-06 18:56:54 +0000

    QL Static vs Dynamic Results

---
+++ Single-Provider-Federation/v1.5-no-constraint/QL-Static-Dynamic/results.sh
@@ -0,0 +1,7 @@
+../../out_to_json_QLs_DPs.sh Episode_Pro episode "Average Profit"  profit.json
+../../out_to_json_QLs_DPs.sh Episode_Fede episode "Federation Rate" federation.json
+../../out_to_json_QLs_DPs.sh Episode_Acc episode "Acceptance Rate" acceptance.json
+../../res_plotter_QLs_DPs.py profit
+../../res_plotter_QLs_DPs.py federation
+../../res_plotter_QLs_DPs.py acceptance
+
