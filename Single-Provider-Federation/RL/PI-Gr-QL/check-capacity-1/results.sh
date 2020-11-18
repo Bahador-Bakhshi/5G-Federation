51b936c927b1724e1fde7acb7cd5591b5f1b029b
Bahador Bakhshi <bakhshi@gmail.com>
2020-11-18 09:13:46 +0000

    RL results

--- Single-Provider-Federation/RL/PI-Gr-QL/check-capacity-1/results.sh
+++ Single-Provider-Federation/RL/PI-Gr-QL/check-capacity-1/results.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-../../out_to_json_rand_DPs.sh Capacity_Profi capacity "Average Profit" profit.json
-../../out_to_json_rand_DPs.sh Capacity_Fede capacity "Federation Rate" federation.json
-../../out_to_json_rand_DPs.sh Capacity_Acce capacity "Acceptance Rate" acceptance.json
+../../out_to_json_gr_dp_ql_rl.sh Capacity_Profi capacity "Average Profit" profit.json
+../../out_to_json_gr_dp_ql_rl.sh Capacity_Fede capacity "Federation Rate" federation.json
+../../out_to_json_gr_dp_ql_rl.sh Capacity_Acce capacity "Acceptance Rate" acceptance.json
 
 
-../../res_plotter_rand_DPs.py profit
-../../res_plotter_rand_DPs.py federation
-../../res_plotter_rand_DPs.py acceptance
+../../res_plotter_gr_dp_ql_rl.py profit
+../../res_plotter_gr_dp_ql_rl.py federation
+../../res_plotter_gr_dp_ql_rl.py acceptance
 
-../../gap_plotter_rand_DPs.py profit
+../../gap_plotter_gr_dp_ql_rl.py profit
 
