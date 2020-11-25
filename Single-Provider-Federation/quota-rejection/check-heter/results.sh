b0a47d257be06a3296dcb0103b98f4b55527fe1b
Bahador-Bakhshi <bakhshi@gmail.com>
2020-11-25 12:33:35 +0000

    check heter results

--- Single-Provider-Federation/quota-rejection/check-heter/results.sh
+++ Single-Provider-Federation/quota-rejection/check-heter/results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-../out_to_json_gr_dp_ql_rl.sh Capacity_Profi capacity "Average Profit" profit.json
-../out_to_json_gr_dp_ql_rl.sh Capacity_Fede capacity "Federation Rate" federation.json
-../out_to_json_gr_dp_ql_rl.sh Capacity_Acce capacity "Acceptance Rate" acceptance.json
+../out_to_json_gr_dp_ql_rl.sh Scale_Profi eta "Average Profit" profit.json
+../out_to_json_gr_dp_ql_rl.sh Scale_Fede capacity "Federation Rate" federation.json
+../out_to_json_gr_dp_ql_rl.sh Scale_Acce capacity "Acceptance Rate" acceptance.json
 
 
 ../res_plotter_gr_dp_ql_rl.py profit
