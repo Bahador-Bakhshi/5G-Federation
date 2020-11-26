1e228d3e8f7009ec4716b86029b5bd94330c2e96
Bahador-Bakhshi <bakhshi@gmail.com>
2020-11-26 11:09:42 +0000

    episode results updated

--- Single-Provider-Federation/quota-rejection/res_plotter_gr_dp_2ql_rl.py
+++ Single-Provider-Federation/quota-rejection/res_plotter_gr_dp_2ql_rl.py
@@ -19,7 +19,12 @@ y_label = res["y_label"]
 results = res["results"]
 
 x = []
+greedy_000 = []
+greedy_050 = []
 greedy_100 = []
+DP_05 = []
+DP_30 = []
+DP_60 = []
 DP_95 = []
 QL_09  = []
 QL_05  = []
@@ -27,7 +32,12 @@ RL  = []
 
 for data in results:
     x.append(data["x"])
+    greedy_000.append(data["greedy000"])
+    greedy_050.append(data["greedy050"])
     greedy_100.append(data["greedy100"])
+    DP_05.append(data["DP_05"])
+    DP_30.append(data["DP_30"])
+    DP_60.append(data["DP_60"])
     DP_95.append(data["DP_95"])
     QL_09.append(data["QL_09"])
     QL_05.append(data["QL_05"])
@@ -38,15 +48,19 @@ fig, ax = plt.subplots()
 plt.grid(linestyle="--", linewidth=0.5)
 plt.xlabel(x_label)
 plt.ylabel(y_label)
-axes = plt.gca()
-axes.set_ylim([45,75])
 
-
+#plt.plot(x, DP_05, label='DP-0.05', color='C1', linestyle='solid', linewidth=1)
+#plt.plot(x, DP_30, label='DP-0.30', color='C2', linestyle='solid', linewidth=1)
+#plt.plot(x, DP_60, label='DP-0.60', color='C3', linestyle='solid', linewidth=1)
 plt.plot(x, DP_95, label='DP', color='C4', linestyle='solid', linewidth=1)
 
 plt.plot(x, QL_09, label='QL-0.9', color='C5', linestyle='dashed', linewidth=1)
-plt.plot(x, QL_05, label='QL-0.5', color='C6', linestyle='dashed', linewidth=1)
+plt.plot(x, QL_09, label='QL-0.5', color='C6', linestyle='dashed', linewidth=1)
 plt.plot(x, RL, label='RL', color='C9', linestyle='-.', linewidth=1)
+
+#plt.plot(x, greedy_000, label='Greedy-0.0', color='C6', linestyle='dashdot', linewidth=1)
+#plt.plot(x, greedy_050, label='Greedy-0.5', color='C7', linestyle='dashdot', linewidth=1)
+plt.plot(x, greedy_100, label='Greedy', color='C8', linestyle='dashdot', linewidth=1)
 
 plt.legend(loc='best', handlelength=4)
 
