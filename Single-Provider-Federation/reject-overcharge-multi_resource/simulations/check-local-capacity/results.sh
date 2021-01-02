add440466041234ff33e435592912207d68be03b
Bahador-Bakhshi <bakhshi@gmail.com>
2021-01-02 09:19:39 +0000

    reject + overcharge: LC results

--- Single-Provider-Federation/reject-overcharge-multi_resource/simulations/check-local-capacity/results.sh
+++ Single-Provider-Federation/reject-overcharge-multi_resource/simulations/check-local-capacity/results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-../out_to_json_gr_dp_2ql_rl.sh Capacity_Profi LC "Average Profit" profit.json
-../out_to_json_gr_dp_2ql_rl.sh Capacity_Fede LC "Federation Rate" federation.json
-../out_to_json_gr_dp_2ql_rl.sh Capacity_Acce LC "Acceptance Rate" acceptance.json
+../out_to_json_gr_dp_2ql_rl.sh Scale_Profit LC "Average Profit" profit.json
+../out_to_json_gr_dp_2ql_rl.sh Scale_Fede LC "Federation Rate" federation.json
+../out_to_json_gr_dp_2ql_rl.sh Scale_Acce LC "Acceptance Rate" acceptance.json
 
 
 ../res_plotter_gr_dp_2ql_rl.py profit
