5533025ef5837b3a0f1bd6374656ac4ef8940ac0
Bahador-Bakhshi <bakhshi@gmail.com>
2021-01-14 15:26:11 +0000

    results

--- Single-Provider-Federation/reject-overcharge-multi_resource/simulations/check-offered-load/results.sh
+++ Single-Provider-Federation/reject-overcharge-multi_resource/simulations/check-offered-load/results.sh
@@ -3,11 +3,13 @@
 ../out_to_json_gr_dp_2ql_rl.sh Load_Profi l "Average Profit" profit.json
 ../out_to_json_gr_dp_2ql_rl.sh Load_Fede l "Federation Rate" federation.json
 ../out_to_json_gr_dp_2ql_rl.sh Load_Acce l "Acceptance Rate" acceptance.json
+../out_to_json_gr_dp_2ql_rl.sh Load_Reject l "Acceptance Rate" reject.json
 
 
 ../res_plotter_gr_dp_2ql_rl.py profit
 ../res_plotter_gr_dp_2ql_rl.py federation
 ../res_plotter_gr_dp_2ql_rl.py acceptance
+../res_plotter_gr_dp_2ql_rl.py reject
 
 ../gap_plotter_gr_dp_2ql_rl.py profit
 
