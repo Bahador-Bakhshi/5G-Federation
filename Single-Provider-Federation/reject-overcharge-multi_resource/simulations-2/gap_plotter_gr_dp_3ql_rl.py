ab369a288d8ad1167c2a573f1a6c16c03ad8aeea
Bahador-Bakhshi <bakhshi@gmail.com>
2021-02-16 09:53:38 +0000

    simulation results

--- Single-Provider-Federation/reject-overcharge-multi_resource/simulations-2/gap_plotter_gr_dp_3ql_rl.py
+++ Single-Provider-Federation/reject-overcharge-multi_resource/simulations-2/gap_plotter_gr_dp_3ql_rl.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-
 import json
 from numpy import *
 import math
 import matplotlib
 import matplotlib.pyplot as plt
 import sys
+import fig_plotter
 
 if len(sys.argv) < 2:
     print("Usage: ", str(sys.argv[0]), "input_file")
@@ -71,51 +71,5 @@ for data in results:
     QL_20.append(QL_20_tmp)
     RL.append(RL_tmp)
 
-
-font = {
-        'weight' : 'bold',
-        'size'   : 12}
-
-matplotlib.rc('font', **font)
-plt.rcParams["axes.labelweight"] = "bold"
-
-fig, ax = plt.subplots()
-plt.grid(linestyle="--", linewidth=0.5)
-
-if x_label == "episode":
-    plt.xlabel(r'$n$')
-elif x_label == "LC":
-    plt.xlabel(r'$\eta_{C^{l}}$')
-elif x_label == "PC":
-    plt.xlabel(r'$\eta_{C^{p}}$')
-elif x_label == "FC":
-    plt.xlabel(r'$\eta_{\Sigma}$')
-elif x_label == "Omega":
-    plt.xlabel(r'$\eta_{\omega}$')
-elif x_label == "Theta":
-    plt.xlabel(r'$\eta_{\theta}$')
-elif x_label == "Lambda":
-    plt.xlabel(r'$\eta_{\lambda}$')
-else:
-    print("Unknow axis X")
-    sys.exit(-1)
-
-plt.ylabel("Gap")
-
-
-plt.plot(x, DP_99, label='DP', color='k', linestyle='solid', linewidth=2, marker = '*')
-
-plt.plot(x, QL_95, label='QL-0.95', color='c', linestyle='dashed', linewidth=2, marker = '^')
-plt.plot(x, QL_55, label='QL-0.55', color='m', linestyle='dashed', linewidth=2, marker = '>')
-plt.plot(x, QL_20, label='QL-0.20', color='y', linestyle='dashed', linewidth=2, marker = 'v')
-
-plt.plot(x, RL, label='RL', color='r', linestyle='-.', linewidth=2, marker = 'D')
-
-plt.plot(x, greedy_100, label='Greedy', color='g', linestyle='dashdot', linewidth=2, marker = '+')
-
-plt.legend(loc='best', handlelength=4)
-
-
-plt.savefig("gap_"+str(sys.argv[1])+".png")
-
+fig_plotter.plotter(x_label, "Gap", x, greedy_000, greedy_050, greedy_100, DP_05, DP_30, DP_60, DP_99, QL_95, QL_55, QL_20, RL, "gap")
 
