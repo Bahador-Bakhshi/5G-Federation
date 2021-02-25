a18d26e4c1bd1e906af3db1dd422d6e7aa086538
Bahador-Bakhshi <bakhshi@gmail.com>
2021-02-25 10:01:45 +0000

    TF-Agent Based DRL

---
+++ Single-Provider-Federation/quota-rejection-TF-agents/DDQL.py
@@ -0,0 +1,328 @@
+import sys
+import sklearn
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import optimizers
+import numpy as np
+import os
+import gym
+import itertools 
+from collections import deque
+from collections import defaultdict 
+import Environment
+
+class Memory_Entity:
+    def __init__(self, state, action, reward, next_state):
+        self.state = state
+        self.action = action
+        self.reward = reward
+        self.next_state = next_state
+
+    def __str__(self):
+        return str(self.state) +","+ str(self.action) +","+ str(self.reward) +","+ str(self.next_state)
+
+
+def print_deque(q):
+    for e in q:
+        print("\t", e)
+
+
+class DQNAgent:
+    def __init__(self, state_size, action_size, learning_rate):
+        self.fit_individual = True
+        self.epochs = 1
+        self.state_size = state_size
+        self.lr = learning_rate
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.model = self._build_model()
+        #self.target = self.model 
+        self.target = self._build_model()
+        self.history = []
+        self.state_importance = None
+    
+    def reset_importance(self):
+        self.state_importance = defaultdict(lambda: 1)
+
+    def _build_model(self):  # Neural Net for Deep-Q learning Model
+
+        keras.backend.clear_session()
+        model = keras.models.Sequential()
+        model.add(keras.layers.Dense(16, activation="elu", input_shape = [self.state_size]))
+        model.add(keras.layers.Dense(16, activation="elu"))
+        model.add(keras.layers.Dense(16, activation="elu"))
+        model.add(keras.layers.Dense(16, activation="elu"))
+        model.add(keras.layers.Dense(self.action_size))
+   
+        print("learning_rate = ", self.lr)
+        model.compile(loss='mse', optimizer=optimizers.Adam(lr=self.lr))
+        
+        print("Model Summary:")
+        print(model.summary())
+
+        return model
+
+
+    def remember(self, state, action, reward, next_state):
+        new_entry = Memory_Entity(object_to_array_state(state), action, reward, object_to_array_state(next_state))
+        try:
+            index = self.memory.index(new_entry) 
+        except ValueError:
+            #print("Adding new_entry")
+            self.memory.append(new_entry)
+        except:
+            print("Error")
+            sys.exit(-1)
+
+
+    def sample(self, batch_size):
+        indices = np.random.randint(len(self.memory), size=batch_size)
+        batch = [self.memory[index] for index in indices]
+        states = np.array([experience.state for experience in batch])
+        actions = np.array([experience.action for experience in batch])
+        rewards = np.array([experience.reward for experience in batch])
+        next_states = np.array([experience.next_state for experience in batch])
+        return states, actions, rewards, next_states
+
+
+    def act(self, state, epsilon):
+        va = Environment.get_valid_actions(state)
+        valid_action_size = len(va)
+
+        if np.random.rand() <= epsilon:
+            rand_index = np.random.randint(valid_action_size)
+            return va[rand_index], True
+
+        act_values = self.model.predict(np.array([object_to_array_state(state)]))
+        
+        #print("1) state = ", state, ", act_values = ", act_values)
+        for a in Environment.Actions:
+            if not (a in va):
+                act_values[0][a] = -1 * np.inf
+
+        #print("2) state = ", state, ", act_values = ", act_values)
+
+        return np.argmax(act_values[0]), False
+
+
+    def unify_q_values(self, states, actions, next_states, updated_target_Q_values):
+        #for p in self.state_importance.keys():
+        #    print("pair = ", p, ", importance = ", self.state_importance[p])
+
+        for i in range(len(states)):
+            for j in range(i+1, len(states)):
+                if np.all(states[i] -  states[j] == 0): 
+                    updated_target_Q_values[j] = updated_target_Q_values[i]
+
+                '''
+                if i != j:
+                    print("states[",i,"] = ", states[i])
+                    print("states[",j,"] = ", states[j])
+                    print("Q[",i,"] = ", updated_target_Q_values[i])
+                    print("Q[",j,"] = ", updated_target_Q_values[j])
+
+                    if np.all(states[i] -  states[j] == 0): 
+                        
+                        print("Equal")
+                        print("actions[",i,"] = ", actions[i])
+                        print("actions[",j,"] = ", actions[j])
+                        
+                        if actions[i] != actions[j]:
+                            updated_target_Q_values[i][actions[j]] = updated_target_Q_values[j][actions[j]]
+                            updated_target_Q_values[j][actions[i]] = updated_target_Q_values[i][actions[i]]
+                        else:
+                            pair_i = tuple(zip(tuple(states[i]), tuple(next_states[i])))
+                            importance_i = 1 + self.state_importance[pair_i]
+
+                            pair_j = tuple(zip(tuple(states[j]), tuple(next_states[j])))
+                            importance_j = 1 + self.state_importance[pair_j]
+
+                            weighted_average = (importance_i * updated_target_Q_values[i][actions[i]] + importance_j * updated_target_Q_values[j][actions[j]]) / (1.0 * importance_i + importance_j)
+
+                            updated_target_Q_values[i][actions[i]] = updated_target_Q_values[j][actions[j]] = weighted_average
+
+                        for k in range(len(updated_target_Q_values[i])):
+                            if k != actions[i] and k != actions[j]:
+                                updated_target_Q_values[i][k] = updated_target_Q_values[j][k] = 0.5 * (updated_target_Q_values[i][k] + updated_target_Q_values[j][k])
+                    
+                    print("Q[",i,"] = ", updated_target_Q_values[i])
+                    print("Q[",j,"] = ", updated_target_Q_values[j])
+                    '''
+
+    def train(self, batch_size, discount_rate, use_target):
+        experiences = self.sample(batch_size)
+        states, actions, rewards, next_states = experiences
+        updated_target_Q_values = np.zeros(shape=(batch_size, self.action_size))
+
+        individual_history = []
+
+        for i in range(batch_size):
+            print("train: experience = ")
+            print("\t (s, a, r, s') = ", states[i], actions[i], rewards[i], next_states[i])
+   
+            next_Q_values = self.model.predict(next_states[i][np.newaxis])
+            va = Environment.get_valid_actions(array_to_object_state(next_states[i]))
+            for a in Environment.Actions:
+                if not (a in va):
+                    next_Q_values[0][a] = -1 * np.inf
+            print("\t Q_online [s', .] = ", next_Q_values)
+            
+            best_next_actions = np.argmax(next_Q_values, axis=1)
+            print("\t a' = argmax(Q_online[s',a]) = ", best_next_actions)
+          
+            next_best_Q_values_target_predict = 0
+            if use_target:
+                next_best_Q_values_target_predict = self.target.predict(next_states[i][np.newaxis])
+            print("\t Q_target [s', .] = ", next_best_Q_values_target_predict)
+
+            next_mask = tf.one_hot(best_next_actions, self.action_size).numpy()
+            next_best_Q_values = ((next_best_Q_values_target_predict) * next_mask).sum(axis=1)
+            print("\t Q_target [s', a'] = ", next_best_Q_values)
+
+            observed_target_Q_values = (rewards[i] + discount_rate * next_best_Q_values)
+            print("\t r + gamma * Q_target [s', a'] = ", observed_target_Q_values)
+ 
+            old_Q_values = self.model.predict(states[i][np.newaxis])
+            updated_target_Q_values[i] = old_Q_values
+            print("\t Q_online [s, .] = ", updated_target_Q_values[i])
+            
+            updated_target_Q_values[i][actions[i]] = observed_target_Q_values[0]
+            print("\t UPDATED Q_online [s, .] = ", updated_target_Q_values[i])
+            
+            if self.fit_individual:
+                hist = self.model.fit(states[i][np.newaxis], updated_target_Q_values[i][np.newaxis], epochs=self.epochs, verbose=0)
+                individual_history.append(hist.history["loss"][0])
+
+        if self.fit_individual:
+            self.history.append(individual_history)
+
+        else:
+            #print("------------------------------------")
+            #print("states = ", states)
+            #print("actions= ", actions)
+            #print("before unifying: updated_target_Q_values = ", updated_target_Q_values)
+            self.unify_q_values(states, actions, next_states, updated_target_Q_values)
+            #print("after unifying: updated_target_Q_values = ", updated_target_Q_values)
+
+            hist = self.model.fit(states, updated_target_Q_values, epochs=self.epochs, verbose=0)
+            #print("after fit: ")
+            #for s in states:
+            #    print(self.model.predict(s[np.newaxis]))
+
+            self.history.append(hist)
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+
+def object_to_array_state(state):
+    res = np.zeros(Environment.State.state_fields_num)
+    index = 0
+    for i in range(len(state.domains_alives)):
+        for j in range(len(state.domains_alives[i])):
+            res[index] = state.domains_alives[i][j]
+            index += 1
+
+    for i in range(len(state.arrivals_departures)):
+        res[index] = state.arrivals_departures[i]
+        index += 1
+
+    #print("state --> array: state = ", state, ", array = ", res)
+    return res
+
+
+def array_to_object_state(state):
+    res = Environment.State(Environment.total_classes)
+    index = 0
+    
+    for i in range(len(res.domains_alives)):
+        tmp_list = [0] * Environment.total_classes
+        for j in range(len(res.domains_alives[i])):
+            tmp_list[j] = state[index]
+            index += 1
+            
+        res.domains_alives[i] = tuple(tmp_list)
+
+    tmp_list = [0] * Environment.total_classes
+    for i in range(len(res.arrivals_departures)):
+        tmp_list[i] = state[index]
+        index += 1
+
+    res.arrivals_departures = tuple(tmp_list)
+    
+    #print("array --> state: array = ", state, ", state = ", res)
+    return res
+
+
+def ddqLearning(env, num_episodes, gamma0 = 0.99, epsilon0 = 0.8):
+    batch_size   = 32
+    learning_rate= 0.005
+    use_target = False
+    visited_states = []
+
+    agent = DQNAgent(3 * Environment.total_classes, len(Environment.Actions), learning_rate)
+
+    for episode in range(num_episodes):
+        print("episode = ", episode)
+        state = env.reset()
+        agent.reset_importance()
+
+        epsilon = max(epsilon0 - episode / 500, 0.01)
+        gamma = gamma0
+
+        for iteration in itertools.count():
+            #print("t =", iteration, "sate =", state)
+
+            action, random = agent.act(state, epsilon)
+            #print("action = ", action, ", random = ", random)
+            next_state, reward, done = env.step(state, action)
+        
+            if done:
+                break
+
+            #print("next_state =", next_state, "reward =", reward, ", done =", done)
+           
+            agent.remember(state, action, reward, next_state)
+
+            state = next_state
+
+            if not(state in visited_states):
+                visited_states.append(state)
+    
+        if episode > 1:
+            agent.train(batch_size, gamma, use_target)
+    
+        if episode % 5 == 0:
+            print("setting target weights in episode = ", episode)
+            agent.target.set_weights(agent.model.get_weights()) 
+            use_target = True
+   
+
+        loss_sum = 0
+        if agent.fit_individual:
+            print("agent.history = ", agent.history)
+            for h in agent.history:
+                print("h = ", h)
+                for v in h:
+                    loss_sum = loss_sum + v
+        else:
+            for h in agent.history:
+                for v in h.history['loss']:
+                    loss_sum = loss_sum + v
+
+        agent.history = []
+        print("loss = ", loss_sum / 1000.0)
+
+    print("Total Visited States = ", len(visited_states))
+    for i in range(len(visited_states)):
+        print(visited_states[i])
+
+    def policy_function(state):
+        action, random = agent.act(state, epsilon=0.05)
+        return action, random
+
+    return policy_function
+
---
+++ Single-Provider-Federation/quota-rejection-TF-agents/working-01/TF_Agent.py
@@ -0,0 +1,345 @@
+
+from __future__ import absolute_import, division, print_function
+
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+import pyvirtualdisplay
+
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.networks import sequential
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
+from tf_agents.utils import common
+from tf_agents.environments import utils
+
+import TF_Environment
+import parser
+import Environment
+
+
+# ## Hyperparameters
+sim_num = 1000
+num_iterations = 100000 # @param {type:"integer"}
+
+initial_collect_steps = 5000  # @param {type:"integer"}
+collection_per_train = 10
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 32  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+
+num_eval_episodes = 10 # @param {type:"integer"}
+eval_interval = 100 # @param {type:"integer"}
+
+
+def check_env(env):
+    env.reset()
+    
+    #Some checks for env
+    print('Observation Spec:')
+    print(env.time_step_spec().observation)
+
+    print('Reward Spec:')
+    print(env.time_step_spec().reward)
+
+    print('Action Spec:')
+    print(env.action_spec())
+
+    time_step = env.reset()
+    print('Time step:')
+    print(time_step)
+
+    action = np.array(0, dtype=np.int32)
+
+    next_time_step = env.step(action)
+    print('Next time step:')
+    print(next_time_step)
+
+
+def create_env():
+    env_validation_episodes = 5
+
+    train_py_env = TF_Environment.TF_Agent_Env_Wrapper(sim_num =  sim_num)
+    #utils.validate_py_environment(train_py_env, episodes = env_validation_episodes)
+    eval_py_env = TF_Environment.TF_Agent_Env_Wrapper(sim_num = sim_num)
+    #utils.validate_py_environment(eval_py_env, episodes= env_validation_episodes)
+
+    train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+    eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+    return train_env, eval_env
+
+
+def create_DQN_agent(train_env):
+
+
+    fc_layer_params = (100, 50)
+    action_tensor_spec = tensor_spec.from_spec(train_env.action_spec())
+    num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
+
+    # Define a helper function to create Dense layers configured with the right
+    # activation and kernel initializer.
+    def dense_layer(num_units):
+        return tf.keras.layers.Dense(
+                    num_units,
+                    activation=tf.keras.activations.relu,
+                    kernel_initializer=tf.keras.initializers.VarianceScaling(
+                        scale=2.0, 
+                        mode='fan_in', 
+                        distribution='truncated_normal'
+                    )
+                )
+
+    # QNetwork consists of a sequence of Dense layers followed by a dense layer
+    # with `num_actions` units to generate one q_value per available action as
+    # it's output.
+    dense_layers = [dense_layer(num_units) for num_units in fc_layer_params]
+
+    q_values_layer = tf.keras.layers.Dense(
+                        num_actions,
+                        activation=None,
+                        kernel_initializer=tf.keras.initializers.RandomUniform(
+                                                minval=-0.03, 
+                                                maxval=0.03
+                                    ),
+                        bias_initializer=tf.keras.initializers.Constant(-0.2)
+                    )
+
+    q_net = sequential.Sequential(dense_layers + [q_values_layer])
+
+    '''
+    
+    fc_layer_params = (100,)
+
+    q_net = q_network.QNetwork(
+            train_env.observation_spec(),
+            train_env.action_spec(),
+            fc_layer_params=fc_layer_params
+        )
+    '''
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    train_step_counter = tf.Variable(0)
+
+    agent = dqn_agent.DqnAgent(
+                train_env.time_step_spec(),
+                train_env.action_spec(),
+                q_network=q_net,
+                optimizer=optimizer,
+                td_errors_loss_fn=common.element_wise_squared_loss,
+                train_step_counter=train_step_counter
+            )
+
+    agent.initialize()
+
+    return agent
+
+
+def create_policies():
+    '''     
+    Agents contain two policies: 
+
+     -   `agent.policy` — The main policy that is used for evaluation and deployment.
+     -   `agent.collect_policy` — A second policy that is used for data collection.
+    '''
+
+    eval_policy = agent.policy
+    collect_policy = agent.collect_policy
+
+    return eval_policy, collect_policy
+
+
+def create_random_policy(train_env):
+    random_policy = random_tf_policy.RandomTFPolicy(
+                                    train_env.time_step_spec(),
+                                    train_env.action_spec()
+                                )
+
+    return random_policy
+
+
+def compute_avg_return(environment, policy, num_episodes=5):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+
+def create_replay_buffer(agent, train_env):
+    replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+                                data_spec=agent.collect_data_spec,
+                                batch_size=train_env.batch_size,
+                                max_length=replay_buffer_max_length
+                            )
+
+    return replay_buffer
+
+
+def collect_step(environment, policy, buffer):
+  time_step = environment.current_time_step()
+  action_step = policy.action(time_step)
+  next_time_step = environment.step(action_step.action)
+  traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+  # Add trajectory to the replay buffer
+  buffer.add_batch(traj)
+
+
+def create_collect_driver(train_env, agent, replay_buffer):
+    replay_buffer_observer = replay_buffer.add_batch
+    driver = dynamic_step_driver.DynamicStepDriver(
+                train_env,
+                agent.collect_policy,
+                observers=[replay_buffer_observer], #FIXME + train_metrics,
+                num_steps=collection_per_train
+            )
+
+    return driver
+
+
+def create_init_collect_driver(train_env, agent, replay_buffer):
+    initial_collect_policy = create_random_policy(train_env)
+    replay_buffer_observer = replay_buffer.add_batch
+
+    driver = dynamic_step_driver.DynamicStepDriver(
+                train_env,
+                initial_collect_policy,
+                observers=[replay_buffer_observer], 
+                num_steps=initial_collect_steps
+            )
+
+    return driver
+
+
+def create_iterator(replay_buffer):
+    dataset = replay_buffer.as_dataset(
+                num_parallel_calls=3, 
+                sample_batch_size=batch_size, 
+                num_steps=2
+            ).prefetch(3)
+    
+    iterator = iter(dataset)
+    return iterator
+
+
+def train(agent, train_env, eval_env):
+    try:
+        get_ipython().run_line_magic('time', '')
+    except:
+        pass
+
+    replay_buffer = create_replay_buffer(agent, train_env)
+    print("replay_buffer is created ")
+    
+    init_driver = create_init_collect_driver(train_env, agent, replay_buffer)
+    print("init_driver is created")
+    
+    init_driver.run()
+    print("init_driver run")
+    
+    iterator = create_iterator(replay_buffer)
+    print("iterator is created")
+
+    collect_driver = create_collect_driver(train_env, agent, replay_buffer)
+    print("collect_driver is created")
+
+    agent.train = common.function(agent.train)
+    collect_driver.run = common.function(collect_driver.run)
+
+    # Reset the train step
+    agent.train_step_counter.assign(0)
+
+    # Evaluate the agent's policy once before training.
+    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+    print("avg_return at begging = ", avg_return)
+    returns = [avg_return]
+
+    for _ in range(num_iterations):
+    
+        time_step = None
+        policy_state = None 
+
+        time_step, policy_state = collect_driver.run(time_step, policy_state)
+        trajectories, buffer_info = next(iterator)
+        train_loss = agent.train(trajectories)
+
+        step = agent.train_step_counter.numpy()
+
+        if step % log_interval == 0:
+            print('step = {0}: loss = {1}'.format(step, train_loss))
+
+        if step % eval_interval == 0:
+            avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+            print('step = {0}: Average Return = {1}'.format(step, avg_return))
+            returns.append(avg_return)
+
+
+def main():
+    train_env, eval_env = create_env()
+    agent = create_DQN_agent(train_env)
+    train(agent, train_env, eval_env)
+    return agent
+
+
+def evaluate_agent(agent, demands):
+    print("\n\n\n\n\n\n")
+    print("------------- Evaluating -------------")
+    test_py_env = TF_Environment.TF_Agent_Env_Wrapper(sim_num = sim_num, requests = demands)
+    test_env = tf_py_environment.TFPyEnvironment(test_py_env)
+
+    num_episodes = 10
+    total_return = 0.0
+    for _ in range(num_episodes):
+        time_step = test_env.reset()
+        episode_return = 0.0
+
+        while not time_step.is_last():
+            action_step = agent.policy.action(time_step)
+            time_step = test_env.step(action_step.action)
+            episode_return += time_step.reward
+        
+        total_return += episode_return
+
+    avg_return = total_return / num_episodes
+    avg_return = avg_return.numpy()[0] / len(demands)
+    print("Total Return = ", avg_return)
+
+    return avg_return, 0, 0
+
+
+
+
+if __name__ == "__main__":
+    parser.parse_config("config.json")
+    agent = main()
+    demands = Environment.generate_req_set(sim_num)
+    evaluate_agent(agent, demands)
+
