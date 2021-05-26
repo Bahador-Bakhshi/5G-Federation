36b7029ff26bf74bed12448c817c80114ba3870b
Bahador-Bakhshi <bakhshi@gmail.com>
2021-05-26 11:34:28 +0000

    ACSF implementation new policy

--- Single-Provider-Federation/ACSF-Implementation-multi-resource/simulations-2/check-local-capacity/process.sh
+++ Single-Provider-Federation/ACSF-Implementation-multi-resource/simulations-2/check-local-capacity/process.sh
@@ -8,5 +8,5 @@ fi
 in_file=$1
 out_file="$1-proc"
 
-echo "l1,l2,f1,f2,a1,a2" > $out_file
+echo "l1, l2, f1, f2, a1, a2, action" > $out_file
 cat $in_file | sed 's/,/ /g' | sed 's/\[(//g' | sed 's/)(/ /g' | sed 's/)\]//g' | sed 's/(/ /g' | sed 's/)//g' | sed 's/://g' | sed 's/Actions.//' | sed 's/  / /g' | sed 's/  / /g' | sed 's/ /,/g' >> $out_file
