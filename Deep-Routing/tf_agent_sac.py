e00b9b6d54b748e259ec4570008b27a912a0ebe6
Bahador-Bakhshi <bakhshi@gmail.com>
2021-05-28 21:43:41 +0000

    SAC only for continuous action space

---
+++ Deep-Routing/tf_agent_sac.py
@@ -0,0 +1,429 @@
+
+import base64
+import imageio
+import IPython
+import matplotlib.pyplot as plt
+import os
+import reverb
+import tempfile
+import PIL.Image
+
+import tensorflow as tf
+
+from tf_agents.agents.ddpg import critic_network
+from tf_agents.agents.sac import sac_agent
+from tf_agents.agents.sac import tanh_normal_projection_network
+from tf_agents.environments import suite_pybullet
+from tf_agents.metrics import py_metrics
+from tf_agents.networks import actor_distribution_network
+from tf_agents.policies import greedy_policy
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.environments import tf_py_environment
+from tf_agents.policies import random_py_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+from tf_agents.train import actor
+from tf_agents.train import learner
+from tf_agents.train import triggers
+from tf_agents.train.utils import spec_utils
+#from tf_agents.train.utils import strategy_utils
+from tf_agents.train.utils import train_utils
+
+tempdir = tempfile.gettempdir()
+
+
+import tf_environment_novalidation
+import parser
+import environment
+import requests
+from graph import debug
+
+# ## Hyperparameters
+req_num = 0
+
+num_iterations = 100000 # @param {type:"integer"}
+
+initial_collect_steps = 10000 # @param {type:"integer"}
+collect_steps_per_iteration = 1 # @param {type:"integer"}
+replay_buffer_capacity = 10000 # @param {type:"integer"}
+
+batch_size = 256 # @param {type:"integer"}
+
+critic_learning_rate = 3e-4 # @param {type:"number"}
+actor_learning_rate = 3e-4 # @param {type:"number"}
+alpha_learning_rate = 3e-4 # @param {type:"number"}
+target_update_tau = 0.005 # @param {type:"number"}
+target_update_period = 1 # @param {type:"number"}
+gamma = 0.99 # @param {type:"number"}
+reward_scale_factor = 1.0 # @param {type:"number"}
+
+actor_fc_layer_params = (256, 256)
+critic_joint_fc_layer_params = (256, 256)
+
+log_interval = 5000 # @param {type:"integer"}
+
+num_eval_episodes = 20 # @param {type:"integer"}
+eval_interval = 10000 # @param {type:"integer"}
+
+policy_save_interval = 5000 # @param {type:"integer"}
+
+
+train_step = train_utils.create_train_step()
+table_name = 'uniform_table'
+
+def check_env(env):
+    env.reset()
+    
+    #Some checks for env
+    print('Observation Spec:')
+    print(env.time_step_spec().observation)
+
+    print('Reward Spec:')
+    print(env.time_step_spec().reward)
+
+    print('Action Spec:')
+    print(env.action_spec())
+
+    time_step = env.reset()
+    print('Time step:')
+    print(time_step)
+
+    action = np.array(0, dtype=np.int32)
+
+    next_time_step = env.step(action)
+    print('Next time step:')
+    print(next_time_step)
+
+
+def create_env(topology, src_dst_list, sfcs_list):
+
+    train_py_env = tf_environment_novalidation.TF_Agent_Env_Wrapper(topology.copy(), src_dst_list, sfcs_list, req_num = req_num)
+    eval_py_env = tf_environment_novalidation.TF_Agent_Env_Wrapper(topology.copy(), src_dst_list, sfcs_list, req_num = req_num)
+
+    train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+    eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+    
+    '''
+    env_name = "MinitaurBulletEnv-v0" # @param {type:"string"}
+    train_env = suite_pybullet.load(env_name)
+    eval_env = suite_pybullet.load(env_name)
+    '''
+
+    return train_env, eval_env
+
+
+def observation_and_action_constraint_splitter(obs):
+	return obs['observations'], obs['valid_actions']
+
+
+def create_SAC_agent(collect_env):
+    observation_spec, action_spec, time_step_spec = (spec_utils.get_tensor_specs(collect_env))
+
+    if debug > 3:
+        print("-----------------------------")
+        print("observation_spec = ", observation_spec)
+        print("-----------------------------")
+    
+    #with strategy.scope():
+    critic_net = critic_network.CriticNetwork(
+        (observation_spec, action_spec),
+        observation_fc_layer_params=None,
+        action_fc_layer_params=None,
+        joint_fc_layer_params=critic_joint_fc_layer_params,
+        kernel_initializer='glorot_uniform',
+        last_kernel_initializer='glorot_uniform')
+
+    #with strategy.scope():
+    actor_net = actor_distribution_network.ActorDistributionNetwork(
+      observation_spec,
+      action_spec,
+      fc_layer_params=actor_fc_layer_params,
+      continuous_projection_net=(
+          tanh_normal_projection_network.TanhNormalProjectionNetwork))
+
+
+    agent = sac_agent.SacAgent(
+        time_step_spec,
+        action_spec,
+        actor_network=actor_net,
+        critic_network=critic_net,
+        actor_optimizer=tf.compat.v1.train.AdamOptimizer(
+            learning_rate=actor_learning_rate),
+        critic_optimizer=tf.compat.v1.train.AdamOptimizer(
+            learning_rate=critic_learning_rate),
+        alpha_optimizer=tf.compat.v1.train.AdamOptimizer(
+            learning_rate=alpha_learning_rate),
+        target_update_tau=target_update_tau,
+        target_update_period=target_update_period,
+        td_errors_loss_fn=tf.math.squared_difference,
+        gamma=gamma,
+        reward_scale_factor=reward_scale_factor,
+        train_step_counter=train_step)
+
+    agent.initialize()
+
+    return agent
+
+
+def create_policies(tf_agent):
+    tf_eval_policy = tf_agent.policy
+    eval_policy = py_tf_eager_policy.PyTFEagerPolicy(tf_eval_policy, use_tf_function=True)
+
+    tf_collect_policy = tf_agent.collect_policy
+    collect_policy = py_tf_eager_policy.PyTFEagerPolicy(tf_collect_policy, use_tf_function=True)
+
+    return collect_policy, eval_policy
+
+
+def create_random_policy(collect_env):
+    random_policy = random_py_policy.RandomPyPolicy(collect_env.time_step_spec(), collect_env.action_spec())
+    
+    '''
+    random_policy = random_tf_policy.RandomTFPolicy(
+                                    collect_env.time_step_spec(),
+                                    collect_env.action_spec(),
+                                    emit_log_probability = False,
+                                    observation_and_action_constraint_splitter=observation_and_action_constraint_splitter
+                                )
+    '''
+    return random_policy
+
+
+def create_learner(tf_agent, experience_dataset_fn):
+    saved_model_dir = os.path.join(tempdir, learner.POLICY_SAVED_MODEL_DIR)
+    
+    # Triggers to save the agent's policy checkpoints.
+    learning_triggers = [
+        triggers.PolicySavedModelTrigger(
+            saved_model_dir,
+            tf_agent,
+            train_step,
+            interval=policy_save_interval),
+        triggers.StepPerSecondLogTrigger(train_step, interval=1000),
+    ]
+
+    agent_learner = learner.Learner(
+            tempdir,
+            train_step,
+            tf_agent,
+            experience_dataset_fn,
+            triggers=learning_triggers)
+
+    return agent_learner
+
+def get_eval_metrics(eval_actor):
+  eval_actor.run()
+  results = {}
+  for metric in eval_actor.metrics:
+    results[metric.name] = metric.result()
+  return results
+
+def log_eval_metrics(step, metrics):
+  eval_results = (', ').join(
+      '{} = {:.6f}'.format(name, result) for name, result in metrics.items())
+  print('step = {0}: {1}'.format(step, eval_results))
+
+
+def create_eval_actor(eval_env, eval_policy, train_step):
+    eval_actor = actor.Actor(
+        eval_env,
+        eval_policy,
+        train_step,
+        episodes_per_run=num_eval_episodes,
+        metrics=actor.eval_metrics(num_eval_episodes),
+        summary_dir=os.path.join(tempdir, 'eval'),
+    )
+
+    return eval_actor
+
+
+def compute_avg_return(environment, policy, num_episodes=3):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      #print("action = ", action_step.action, ", time_step = ", time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+
+def create_replay_buffer(tf_agent):
+    table = reverb.Table(
+        table_name,
+        max_size=replay_buffer_capacity,
+        sampler=reverb.selectors.Uniform(),
+        remover=reverb.selectors.Fifo(),
+        rate_limiter=reverb.rate_limiters.MinSize(1))
+
+    reverb_server = reverb.Server([table])
+
+    reverb_replay = reverb_replay_buffer.ReverbReplayBuffer(
+        tf_agent.collect_data_spec,
+        sequence_length=2,
+        table_name=table_name,
+        local_server=reverb_server)
+
+    return reverb_replay
+
+
+def create_collect_driver(train_env, collect_policy, rb_observer):
+    env_step_metric = py_metrics.EnvironmentSteps()
+    collect_actor = actor.Actor(
+        train_env,
+        collect_policy,
+        train_step,
+        steps_per_run=1,
+        metrics=actor.collect_metrics(10),
+        summary_dir=os.path.join(tempdir, learner.TRAIN_DIR),
+        observers=[rb_observer, env_step_metric])
+
+    return collect_actor
+
+def create_rb_observer(reverb_replay):
+    rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
+        reverb_replay.py_client,
+        table_name,
+        sequence_length=2,
+        stride_length=1
+    )
+
+    return rb_observer
+
+
+def create_init_collect_driver(rb_observer, collect_env, random_policy):
+    initial_collect_actor = actor.Actor(
+        collect_env,
+        random_policy,
+        train_step,
+        steps_per_run=initial_collect_steps,
+        observers=[rb_observer])
+
+    return initial_collect_actor
+
+
+def create_iterator(reverb_replay):
+    dataset = reverb_replay.as_dataset(
+            sample_batch_size=batch_size, num_steps=2).prefetch(50)
+
+    experience_dataset_fn = lambda: dataset
+
+    return experience_dataset_fn
+
+
+def train(agent, train_env, eval_env):
+    
+    collect_policy, eval_policy = create_policies(agent)
+    eval_actor = create_eval_actor(eval_env, eval_policy, train_step)
+    metrics = get_eval_metrics(eval_actor)
+
+    log_eval_metrics(0, metrics)
+
+    replay_buffer = create_replay_buffer(agent)
+    if debug > 1:
+        print("replay_buffer is created ")
+   
+    rb_observer = create_rb_observer(replay_buffer)
+    if debug > 1:
+        print("rb_observer = ", rb_observer)
+
+    random_policy = create_random_policy(train_env)
+    init_driver = create_init_collect_driver(rb_observer, train_env, random_policy)
+    if debug > 1:
+        print("init_driver is created")
+    
+    init_driver.run()
+    if debug > 1:
+        print("init_driver run")
+    
+    iterator = create_iterator(replay_buffer)
+    if debug > 1:
+        print("iterator is created")
+
+    collect_actor = create_collect_driver(train_env, collect_policy, rb_observer)
+    if debug > 1:
+        print("collect_driver is created")
+
+    #agent.train = common.function(agent.train)
+    #collect_driver.run = common.function(collect_driver.run)
+
+    # Reset the train step
+    agent.train_step_counter.assign(0)
+
+    # Evaluate the agent's policy once before training.
+    avg_return = get_eval_metrics(eval_actor)["AverageReturn"]
+    returns = [avg_return]
+
+    agent_learner = create_learner(agent, iterator)
+
+    for _ in range(num_iterations):
+        # Training.
+        collect_actor.run()
+        loss_info = agent_learner.run(iterations=1)
+
+        # Evaluating.
+        step = agent_learner.train_step_numpy
+
+        if eval_interval and step % eval_interval == 0:
+            metrics = get_eval_metrics(eval_actor)
+            log_eval_metrics(step, metrics)
+            returns.append(metrics["AverageReturn"])
+
+        if log_interval and step % log_interval == 0:
+            print('step = {0}: loss = {1}'.format(step, loss_info.loss.numpy()))
+
+    rb_observer.close()
+    reverb_server.stop()
+
+
+def main(topology, src_dst_list, sfcs_list):
+    
+    train_env, eval_env = create_env(topology, src_dst_list, sfcs_list)
+
+    agent = create_SAC_agent(train_env)
+    train(agent, train_env, eval_env)
+
+    return agent
+
+
+def evaluate_agent(topology, src_dst_list, sfcs_list, agent, demands):
+    test_py_env = tf_environment_novalidation.TF_Agent_Env_Wrapper(topology.copy(), src_dst_list, sfcs_list, req_num=req_num, requests=demands)
+    test_env = tf_py_environment.TFPyEnvironment(test_py_env)
+
+    num_episodes = 1
+    total_return = 0
+    for _ in range(num_episodes):
+        time_step = test_env.reset()
+        episode_return = 0
+
+        while not time_step.is_last():
+            action_step = agent.policy.action(time_step)
+            time_step = test_env.step(action_step.action)
+            episode_return += time_step.reward
+        
+        total_return += episode_return
+    
+    #aavg_return = total_return / num_episodes
+    #avg_return = avg_return.numpy()[0] / len(demands)
+    #return avg_return, 0, 0
+    
+    return (total_return.numpy()[0])/num_episodes
+
+
+if __name__ == "__main__":
+    topology = parser.generate_topo("topo_10_1.json")
+    parser.parse_sfc_config("config.json")
+    src_dst_list, req_num, sfcs_list = requests.generate_traffic_load_config(topology)
+    agent = main(topology, src_dst_list, sfcs_list)
+
+    demands = Environment.generate_req_set(req_num)
+    evaluate_agent(agent, demands)
+
