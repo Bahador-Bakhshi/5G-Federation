d2ef9b59ddad9bb0bacf2b3a1955e95e647e0582
Bahador-Bakhshi <bakhshi@gmail.com>
2021-06-09 22:14:23 +0000

    PPO is not much better in 10 node

--- Deep-Routing/run.sh
+++ Deep-Routing/run.sh
@@ -2,7 +2,7 @@ nohup ./tester.py > res.out-tmp-1 &
 nohup ./tester.py > res.out-tmp-2 &
 nohup ./tester.py > res.out-tmp-3 &
 nohup ./tester.py > res.out-tmp-4 &
-nohup ./tester.py > res.out-tmp-5 &
-nohup ./tester.py > res.out-tmp-6 &
-nohup ./tester.py > res.out-tmp-7 &
-nohup ./tester.py > res.out-tmp-8 &
+#nohup ./tester.py > res.out-tmp-5 &
+#nohup ./tester.py > res.out-tmp-6 &
+#nohup ./tester.py > res.out-tmp-7 &
+#nohup ./tester.py > res.out-tmp-8 &
