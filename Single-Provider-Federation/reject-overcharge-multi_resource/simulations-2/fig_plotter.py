d20e73a3b71668c12de01bceac01dc8f593a0bed
Bahador <bakhshi@gmail.com>
2021-07-16 16:11:57 +0430

    new graphs

--- Single-Provider-Federation/reject-overcharge-multi_resource/simulations-2/fig_plotter.py
+++ Single-Provider-Federation/reject-overcharge-multi_resource/simulations-2/fig_plotter.py
@@ -53,13 +53,14 @@ def plotter(x_label, y_label, x, greedy_000, greedy_050, greedy_100, DP_05, DP_3
 
     plt.legend(loc='best', handlelength=3, prop={'size': 12})
 
-    x_ticks = [x[i] for i in range(0, len(x) + 1, 2)]
+    x_ticks = [x[i] for i in range(0, len(x) + 1, 2)] #change 2 to 3 for "check_episode"
     ax.set_xticks(x_ticks)
 
-    min_y = min(min(DP_99), min(QL_95), min(QL_55), min(QL_20), min(RL), min(greedy_100))
-    max_y = max(max(DP_99), max(QL_95), max(QL_55), max(QL_20), max(RL), max(greedy_100))
+    min_y = math.floor(min(min(DP_99), min(QL_95), min(QL_55), min(QL_20), min(RL), min(greedy_100)))
+    max_y = math.ceil(max(max(DP_99), max(QL_95), max(QL_55), max(QL_20), max(RL), max(greedy_100)))
     max_yticks = 6
-    step_y = (max_y - min_y) / max_yticks
+    step_y = (max_y - min_y) / (max_yticks * 1.0)
+    print("float step = ", step_y)
 
     if step_y < 1:
         min_y = max(0, round(min_y - step_y / 3, 2))
@@ -67,8 +68,14 @@ def plotter(x_label, y_label, x, greedy_000, greedy_050, greedy_100, DP_05, DP_3
         y_ticks = [min_y + i * step_y for i in range(max_yticks + 1)]
     else:
         min_y = max(0, int(min_y - step_y / 3))
-        step_y = int(round(step_y, 0))
+        step_y = math.ceil(step_y + 0.1)
         y_ticks = [min_y + i * step_y for i in range(max_yticks + 1)]
+
+        print("min = ", min_y)
+        print("max = ", max_y)
+        print("tick= ", y_ticks)
+        print("step= ", step_y)
+        #sys.exit(-1)
 
     ax.set_yticks(y_ticks)
 
--- Single-Provider-Federation/reject-overcharge-multi_resource/simulations-2/res_plotter_gr_dp_3ql_rl.py
+++ Single-Provider-Federation/reject-overcharge-multi_resource/simulations-2/res_plotter_gr_dp_3ql_rl.py
@@ -22,7 +22,7 @@ res = json.load(res_file_handler)
 res_file_handler.close()
 
 x_label = res["x_label"]
-y_label = res["y_label"]
+y_label = res["y_label"] + " %"
 results = res["results"]
 
 x = []
@@ -40,17 +40,18 @@ RL  = []
 
 for data in results:
     x.append(data["x"])
-    greedy_000.append(data["greedy000"])
-    greedy_050.append(data["greedy050"])
-    greedy_100.append(data["greedy100"])
-    DP_05.append(data["DP_05"])
-    DP_30.append(data["DP_30"])
-    DP_60.append(data["DP_60"])
-    DP_99.append(data["DP_99"])
-    QL_95.append(data["QL_95"])
-    QL_55.append(data["QL_55"])
-    QL_20.append(data["QL_20"])
-    RL.append(data["RL"])
+    greedy_000.append(100 * data["greedy000"])
+    greedy_050.append(100 * data["greedy050"])
+    greedy_100.append(100 * data["greedy100"])
+    DP_05.append(100 * data["DP_05"])
+    DP_30.append(100 * data["DP_30"])
+    DP_60.append(100 * data["DP_60"])
+    DP_99.append(100 * data["DP_99"])
+    QL_95.append(100 * data["QL_95"])
+    QL_55.append(100 * data["QL_55"])
+    QL_20.append(100 * data["QL_20"])
+    RL.append(100 * data["RL"])
+
 
 fig_plotter.plotter(x_label, y_label, x, greedy_000, greedy_050, greedy_100, DP_05, DP_30, DP_60, DP_99, QL_95, QL_55, QL_20, RL, str(sys.argv[1]), no_greedy)
 
